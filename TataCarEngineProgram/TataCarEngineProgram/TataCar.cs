7f7611174108dd0ada98d96aef95a1019cd562d2
navyashrees29 <navya9529@gmail.com>
2021-06-27 18:53:08 +0530

    update

---
+++ TataCarEngineProgram/TataCarEngineProgram/TataCar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TataCarEngineProgram
+{
+    class TataCar
+    {
+        IEngine _engine;
+        public TataCar(IEngine engine)
+        {
+            this._engine = engine;
+        }
+
+        public void Drive()
+        {
+            _engine.Start();
+        }
+
+        public void Halt()
+        {
+            _engine.Stop();
+        }
+    }
+}
