29f88538c7ef234e89b65c1718465ff85e7ed320
navyashrees29 <navya9529@gmail.com>
2021-07-06 21:55:16 +0530

    update

--- BMICalculationProgram/ConsoleInputReaderLib/ConsoleInputReaderType.cs
+++ BMICalculationProgram/ConsoleInputReaderLib/ConsoleInputReaderType.cs
@@ -18,7 +18,7 @@ namespace ConsoleInputReaderLib
 
         public static int GetUserWeight()
         {
-            ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Enter your weight in Kg:");
+            ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Enter your weight in KG:");
             string weightInKG = Console.ReadLine();
             int weight = int.Parse(weightInKG);
             return weight;
