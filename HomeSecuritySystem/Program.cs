f47f3a9e1e87d3ad044bf386e86c0ade5c2fec7f
navyashrees29 <navya9529@gmail.com>
2021-07-09 18:48:36 +0530

    upload

---
+++ HomeSecuritySystem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSecuritySystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SecuritySystem securitySystem = new SecuritySystem();
+            DoorSensorStatusSystem sensorSystem = new DoorSensorStatusSystem(securitySystem.Update);
+
+            DoorSensor doorSensor = new DoorSensor();
+            doorSensor.StatusChanged += sensorSystem;
+            doorSensor.Open();
+            doorSensor.Close();
+        }
+    }
+}
